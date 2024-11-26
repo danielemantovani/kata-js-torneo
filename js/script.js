7c9166d91589d499d811a9ab147cf0b04367dbbd
danielemantovani <daniele.mantovani124@gmail.com>
2024-11-26 17:36:20 +0100

    feat: update warriors power after training

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 // ogni combattente sceglierà casualmente un'arma dalla relativa lista. Una volta scelta, un'arma non sarà più disponibile per i successivi combattenti.
 
-// creo un nuovo array con i guerrieri e le armi che gli vengono assegnate
+// creo un nuovo array con i combattenti e le armi che gli vengono assegnate
 const weaponFighter = fighters.map(fighter =>{
 
     // creo una variabile per generare casualmente l'indice di un oggetto nell'array weapons 
@@ -22,6 +22,26 @@ const weaponFighter = fighters.map(fighter =>{
 
 });
 
-console.log(weaponFighter);
+console.log("Array dei combattenti con la loro arma",weaponFighter);
 
+
+// **Milestone 2 - Allenamento:**
+
+// ogni combattente si sottoporrà ad un allenamento che incrementerà (o forse no) la sua potenza, moltiplicandola per un numero casuale tra 1 e 100.
+
+const training = fighters.map(fighter => {
+
+    // genero un numero casuale tra 1 e 100
+    const randomPower = Math.random() * (100 - 1) + 1;
+
+    // aumento la potenza del combattente
+    const newPower = fighter.power * randomPower;
+
+    return {
+        ...fighter, // creo un nuovo oggetto con tutte le proprietà del combattente
+        power: Math.floor(newPower) // imposto la nuova potenza arrotondata
+    };
+});
+
+console.log("Combattenti con potenza aggiornata dopo l'allenamento", training);
 
